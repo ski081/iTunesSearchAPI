c1903637d19da997fc036329f473cd7eb1938628
Mark Struzinski <ski081@gmail.com>
2019-02-20 21:56:42 -0500

    Finishing iTunes search client

--- iTunesSearchAPI/ViewController.swift
+++ iTunesSearchAPI/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var dataTask: URLSessionDataTask? = nil
+    var results: [SearchResult] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    func search<T>(for type: T.Type,
+                   with term: String) where T: MediaType  {
+        let components = AppleiTunesSearchURLComponents<T>(term: term)
+        guard let url = components.url else {
+            fatalError("Error creating url")
+        }
+        
+        self.dataTask?.cancel()
+        self.dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data,
+                error == nil else {
+                    fatalError("Networking error: \(String(describing: error)) \(response)")
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let searchResponse = try decoder.decode(SearchResponse.self, from: data)
+                self.results = searchResponse.nonExplicitResults
+            } catch {
+                fatalError("Error decoding Search Results: \(error)")
+            }
+            
+//            DispatchQueue.main.async {
+//                self.tableView.reloadData()
+//            }
+        }
+        
+        self.dataTask?.resume()
+    }
 }
 
